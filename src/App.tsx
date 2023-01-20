3bbbe0eddb8eb63db010db13f8f3b806927756ad
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-01-20 16:14:35 +0545

    Layout component added to code more readable

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
+import React, { useEffect } from "react";
 import "./App.css";
 import { fetchProduct } from "./redux/productSlice";
 import { useAppDispatch, useAppSelector } from "./redux/hooks";
 import Home from "./components/Home";
 import Products from "./components/Products";
-import { Routes, Route, Link } from "react-router-dom";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
   const { products, isLoading, errMessage } = useAppSelector(
     (state) => state.products
   );
@@ -21,33 +20,20 @@ function App() {
   return (
     <div className="App">
       {errMessage && <div>Error: {errMessage}</div>}
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/product">Product</Link>
-      </div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Products products={products} />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="product" element={<Products products={products} />} />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <div>
+              <h1>PAGE NOT FOUND (404)</h1>
+            </div>
+          }
+        />
       </Routes>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
---
+++ src/components/Layout.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import reactLogo from "../assets/react.svg";
+import "../App.css";
+import { Outlet, Link } from "react-router-dom";
+
+function Layout() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/product">Product</Link>
+      </div>
+      <Outlet />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default Layout;
